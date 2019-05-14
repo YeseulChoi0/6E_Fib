617e70e111ebdda6cd76467478866c9735a233d1
Emma Choi <35973025+YeseulChoi0@users.noreply.github.com>
2019-05-13 21:08:43 -0400

    Update Fib.java

--- Fib.java
+++ Fib.java
@@ -11,10 +11,18 @@ public class Fib {
       @return the nth Fibonacci number
               by implementing the recurrence relation
      */
-    public ?? long fib_recurrence( int n) {
-        return -2; // invalid Fibonacci number
+    public long fib_recurrence( int n) {
+        if (n <= 2)
+          return 1;
+        else{
+          return fib_recurrence(n-1) + fib_recurrence(n-2);
+        }
     }
-     /* These are class / instance methods because...
+     /* These are class methods because...
+        No matter what, the way to calculate a 
+        Fibonacci number using recursion is 
+        always the same and does not depend on 
+        any varying factors. 
     */
 
 
@@ -22,18 +30,30 @@ public class Fib {
       @return the nth Fibonacci number
               calculated via the 8th-grade algorithm
      */
-    public ?? long fib_grade8( int n) {
-        return -2; // invalid Fibonacci number
+    public long fib_grade8( int n) {
+        long beforePrev = 1;
+        long prev = 1;
+        while (n > 1){
+          long saver = prev;
+          prev = beforePrev + prev;
+          beforePrev = saver;
+          n—-;
+        }
+        return beforePrev;
     }
     /* Time complexity:
-       Consider the size of the problem to be ...
+       Consider the size of the problem to be ... n because n
+       calculations must be done in order to find the nth
+       Fibonacci number. 
        
-       As the proxy for the time required, count...
+       As the proxy for the time required, count the number of times 
+       you calculate the next value. 
        
        Then cost of the the recurrence algorithm
-       grows ?? 
+       grows linearly
        as the size of the problem increases,
-       because ??
+       because it only takes one extra step to get 
+       to the next Fibonacci number. 
      */
 
 
@@ -43,7 +63,7 @@ public class Fib {
               Type is double so that this exercise can
               ignore rounding issues.
      */
-    public ?? double fib_Binet( int n) {
+    public double fib_Binet( int n) {
         return -2; // invalid Fibonacci number
     }
     /* Time complexity:
